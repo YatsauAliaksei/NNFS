533f9330a16ab407d01e26ebd3bdb3a767c7fdab
Alexey <yatsew.alexey@gmail.com>
2016-09-26 23:41:21 +0200

    cleaning...

--- src/main/java/org/ml4bull/algorithm/LogisticRegressionCostFunction.java
+++
@@ -1,9 +0,0 @@
-package org.ml4bull.algorithm;
-
-public class LogisticRegressionCostFunction implements CostFunction {
-
-    public void calculate(double[] x, double[] y) {
-
-
-    }
-}
--- src/main/java/org/ml4bull/algorithm/StepFunction.java
+++ src/main/java/org/ml4bull/algorithm/StepFunction.java
@@ -2,19 +2,19 @@ package org.ml4bull.algorithm;
 
 public class StepFunction implements ActivationFunction {
 
-    private double treshold = 0.5;
+    private double threshold = 0.5;
 
     public StepFunction() {
 
     }
 
     public StepFunction(double treshold) {
-        this.treshold = treshold;
+        this.threshold = treshold;
     }
 
     @Override
     public double activate(double value) {
-        if (value >= treshold) {
+        if (value >= threshold) {
             return 1;
         } else
             return 0;
@@ -23,7 +23,7 @@ public class StepFunction implements ActivationFunction {
     public double[] activate(double[] value) {
         double[] r = new double[value.length];
         for (int i = 0; i < value.length; i++) {
-            if (value[i] >= treshold) {
+            if (value[i] >= threshold) {
                 r[i] = 1;
             } else
                 r[i] = 0;
